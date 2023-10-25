85df526a2d4cf087b732d1ddc254962df6d3c2ed
Korniis <132537155+Korniis@users.noreply.github.com>
2023-10-25 22:10:38 +0800

    bew

--- 没有async的异步/Program.cs
+++ 没有async的异步/Program.cs
@@ -2,10 +2,43 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s = await ReadAsync(1);
+            Console.WriteLine(s);
         }
-        
+        /*  public static async Task<string> ReadAsync(int num)
+          {
+              if (num == 1)
+              {
+                  return await File.ReadAllTextAsync(@"d:\ddd.txt");
+              }
+              else if (num == 2)
+              {
+                  return await File.ReadAllTextAsync(@"d:\2.txt");
+              }
+              else
+              {
+                  throw new ArgumentException()
+                  {
+                  };
+              }*/
+        static Task<string> ReadAsync(int num)
+        {
+            if (num == 1)
+            {
+                return File.ReadAllTextAsync(@"d:\ddd.txt");
+            }
+            else if (num == 2)
+            {
+                return File.ReadAllTextAsync(@"d:\2.txt");
+            }
+            else
+            {
+                throw new ArgumentException()
+                {
+                };
+            }
+        }
     }
 }
