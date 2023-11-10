c6c71da46dca7f4dd3189b180096fe9c309b1830
Korniis <132537155+Korniis@users.noreply.github.com>
2023-11-10 20:43:29 +0800

    scop

---
+++ IOC/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace IOC
+{
+    public class TestServiceImpl2 : ITestService
+    {
+        public string Name { get; set; }
+        public void SayHi()
+        {
+            Console.WriteLine($"你好，我是{Name}");
+        }
+    }
+    public interface ITestService
+    {
+        public string Name { get; set; }
+        public void SayHi();
+    }
+    public class TestServiceImpl : ITestService
+    {
+        public string Name { get; set; }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {Name}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddScoped<TestServiceImpl>();
+            using (var service = services.BuildServiceProvider())
+            {
+                TestServiceImpl service3 = service.GetService<TestServiceImpl>();
+                /*
+                                TestServiceImpl service2 =  service.GetService<TestServiceImpl>();
+                                service2.SayHi();*/
+                using (var scope = service.CreateScope())
+                {
+                    TestServiceImpl service2 = scope.ServiceProvider.GetService<TestServiceImpl>();
+                    service2.Name = "qwe";
+                    service2.SayHi();
+                    TestServiceImpl service1 = scope.ServiceProvider.GetService<TestServiceImpl>();
+                    service1.Name = "asdas";
+                    service1.SayHi();
+                    service2.SayHi();
+                    Console.WriteLine(object.ReferenceEquals(service1, service2));
+                    Console.WriteLine(object.ReferenceEquals(service3, service2));
+                }
+            }
+        }
+    }
+}
---
+++ LinqMS/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LinqMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*    string s = "53,33,55,888,9";
+                string[] strs = s.Split(',');
+                var nums = strs.Select(e => Convert.ToInt32(e)).Average();
+
+                Console.WriteLine(nums);*/
+            string s = "asdasdasdasdasdasdas123123sdasdDSAFG a FDADSFSAdASDF SADF SAdf";
+          var items= s.Where(c => char.IsLetter(c))
+                .Select(c => char.ToLower(c))
+                .GroupBy(c => c)
+                .Select(g => new { g.Key,Count=g.Count() })
+                .Where(g => g.Count > 2) 
+                .OrderByDescending(g =>g.Count)
+               ;
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
